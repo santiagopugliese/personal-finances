fd79e7f732a1b8209208d6e287ef794edca4acd0
santiagopugliese <63408326+santiagopugliese@users.noreply.github.com>
2025-09-04 03:05:04 +0000

    fix: remove node_modules and .next from repo, fix glob version.

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,7 @@ const withPWA = nextPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  // no registres SW en dev
-  disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === 'development', // no SW in dev
 })
 
 /** @type {import('next').NextConfig} */
