24b0e626f5d2adf8a701b2e3cb0712cfce8cd922
santiagopugliese <63408326+santiagopugliese@users.noreply.github.com>
2025-09-16 04:05:51 +0000

    Fix deno imports.

--- supabase/functions/blue-rate/index.ts
+++ supabase/functions/blue-rate/index.ts
@@ -1,5 +1,6 @@
+// @ts-nocheck
 import 'jsr:@supabase/functions-js/edge-runtime.d.ts'
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+import { createClient } from '@supabase/supabase-js'
 
 Deno.serve(async () => {
   try {
